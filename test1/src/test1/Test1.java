ea69ee6bbbb373d2ba4309fbe1e197a809bc22f9
destroy <destroy@DESKTOP-D4TT1BF.mshome.net>
2020-04-10 11:12:37 +0800

    1

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,7 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("主线");
 		System.out.println("分支");
-		System.out.println("哦");
 		System.out.println("黑黑恶黑");
 	}
 	//9898989
